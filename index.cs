8ec4e9df94ed5801a9944cc8a858bcc6cd52d235
Thais Karoline <thaiskarolinefernandes@hotmail.com>
2023-06-14 21:26:19 -0300

    MediaCS

---
+++ index.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace programa {
+    class Program {
+        static void Main(string[] args) {
+            double x, y, media;
+
+            Console.Write("Digite o primeiro numero: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo numero: ");
+            y = double.Parse(Console.ReadLine());
+            media = (x + y) / 2.0;
+            Console.WriteLine ("Media =" + media);
+        }
+    }
+}
